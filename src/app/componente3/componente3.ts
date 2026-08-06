ab32834faa5e5c33bb4c07d64801e7a96b3830c6
JDquintero1002 <jdquintero@unicauca.edu.co>
2026-08-06 15:54:40 -0500

    componentes

---
+++ src/app/componente3/componente3.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente3',
+  imports: [],
+  templateUrl: './componente3.html',
+  styleUrl: './componente3.css',
+})
+export class Componente3 {
+
+}
